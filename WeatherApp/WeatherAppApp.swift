181feba3120eedb45a646b1de96739ba44d3af41
brentcrosby <brentobox241@gmail.com>
2025-03-03 18:18:23 -0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Brent Crosby on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
